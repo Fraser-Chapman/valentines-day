5aebaf27636698b66ae6605e4aa9a3bbfc99e39e
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2023-12-25 01:55:54 +0000

    update socket to publish messages it receives back onto the topic

--- back-end/src/main/java/uk/co/fraser/valentines/controller/HugsHandler.java
+++ back-end/src/main/java/uk/co/fraser/valentines/controller/HugsHandler.java
@@ -2,7 +2,6 @@ package uk.co.fraser.valentines.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.AbstractWebSocketMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -20,7 +19,7 @@ public class HugsHandler extends AbstractWebSocketHandler {
 
     @Override
     public void handleMessage(final WebSocketSession session, final WebSocketMessage<?> message) throws Exception {
-        final String payload = (String) message.getPayload();
+        session.sendMessage(message);
 
         super.handleMessage(session, message);
     }
