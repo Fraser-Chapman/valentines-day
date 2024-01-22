b373effd31df43f98899e76294dd1b95a2783739
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-01-22 20:00:19 +0000

    New release

--- docs/_app/immutable/chunks/paths.huAAOrWn.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_nqglh7?.base??"/valentines-day",a=globalThis.__sveltekit_nqglh7?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/paths.w2-gGmqa.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1jekq65?.base??"/valentines-day",e=globalThis.__sveltekit_1jekq65?.assets??s;export{e as a,s as b};
