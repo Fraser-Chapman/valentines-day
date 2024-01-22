c035cae74243a7f8cef44a82e739d6847d4a645b
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-01-22 19:56:22 +0000

    Add show me the plan button which reveals the agenda

--- src/routes/yes/+page.svelte
+++ src/routes/yes/+page.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import bearsHugging from '$lib/assets/bears-hugging.png';
+
+	let soundsGood: boolean = false;
+
+	function soundsGoodClicked(): void {
+		soundsGood = true;
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -8,20 +14,24 @@
 
 		<img src="{bearsHugging}" alt="A bear blowing a kiss">
 
-		<div>
-			<h3 class="h3">Agenda:</h3>
-			<ul>
-				<li>
-					- 14:30 Mini Golf @ Urban Playground
-				</li>
-				<li>
-					- 17:30 Maki & Ramen
-				</li>
-				<li>
-					- Finish @ CloudWater Brewery
-				</li>
-			</ul>
-		</div>
+		{#if soundsGood}
+			<div>
+				<h3 class="h3">Agenda:</h3>
+				<ul>
+					<li>
+						- 14:30 Mini Golf @ Urban Playground
+					</li>
+					<li>
+						- 17:30 Maki & Ramen
+					</li>
+					<li>
+						- Finish @ CloudWater Brewery
+					</li>
+				</ul>
+			</div>
+		{:else }
+			<button class="btn variant-filled" on:click="{soundsGoodClicked}">Show me the plan!</button>
+		{/if}
 
 	</div>
 </div>
